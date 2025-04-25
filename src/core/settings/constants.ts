23ab6521189517de2ff99ade65d8c719b070255d
taraspetrivskyi <petrivskytaras02@gmail.com>
2025-04-25 22:21:28 +0300

    Fix type error

--- src/core/settings/constants.ts
+++ src/core/settings/constants.ts
@@ -3,4 +3,5 @@ import { type SettingsContextProps } from './types';
 export const initialState: SettingsContextProps = {
   themeMode: 'light',
   setThemeMode: () => true,
+  toggleTheme: () => null,
 };
