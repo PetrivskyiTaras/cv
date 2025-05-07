98a7996f8e2f40219be83846b70cc451b09c2957
taraspetrivskyi <petrivskytaras02@gmail.com>
2025-05-07 14:33:54 +0300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { Libre_Baskerville } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 
 import Providers from '@/core/components/Providers';
 
@@ -30,6 +31,7 @@ const RootLayout = ({
             </Providers>
           </AppRouterCacheProvider>
         </main>
+        <Analytics />
       </body>
     </html>
   );
